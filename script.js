b96cfb2ada325dbdf0cf9e5a9f6f56a7d9aa207d
Alex Sanmartín <isanmartin8a@gmail.com>
2022-11-24 14:08:41 +0100

    seguimos

--- script.js
+++ script.js
@@ -83,13 +83,74 @@ btnLogin.addEventListener('click', (event) => {
       currentAccount.owner.split(' ')[0]
     }`
     containerApp.style.opacity = 100
+    // calcDisplaySummary(currentAccount.movements)
+    calcDisplayBalance(currentAccount.movements)
+    displayMovements(currentAccount.movements)
+    startTimer(2, labelTimer);
+
+    btnSort.addEventListener('click', (event) => {
+      const sortedMovements = currentAccount.movements.reverse()
+      if(btnSort.innerHTML == '↓ SORT') {
+        btnSort.innerHTML = '&uparrow; SORT'
+      } else {
+        btnSort.innerHTML = '&downarrow; SORT'
+      }
+      displayMovements(sortedMovements)
+    })
   }
+
   console.log(currentAccount)
 })
 
+
+
+/* calcular y mostrar ingresos totales, retiradas totales e intereses */
+function calcDisplaySummary(movements) {}
+
+/* calcular y mostrar balance */
+function calcDisplayBalance(movements) {
+  const balance = movements.reduce(
+    (acc, cur) => acc + cur,
+     0)
+  labelBalance.textContent = `${balance.toFixed(2)} €`
+
+  const income = movements.reduce((acc, cur) => {
+    if (cur > 0) {
+      return acc + cur
+    }
+    return acc
+  }
+  , 0)
+  labelSumIn.textContent = `${income.toFixed(2)} €`
+
+  const outcome = movements.reduce((acc, cur) => {
+    if (cur < 0) {
+      return acc + cur
+    }
+    return acc
+  }
+  , 0)
+  labelSumOut.textContent = `${Math.abs(outcome).toFixed(2)} €`
+
+  const interest = movements.reduce((acc, cur) => {
+    if (cur > 0) {
+      return acc + cur * 1.2 / 100
+    }
+    return acc
+  }
+  , 0)
+
+  if(interest < 5) {
+    labelSumInterest.textContent = '0.00 €'
+  } else {
+    labelSumInterest.textContent = `${interest.toFixed(2)} €`
+  }
+}
+
+/* mostrar movimientos de la cuenta de un usuario */
 function displayMovements(movements) {
   containerMovements.innerHTML = ''
-
+  let html = ''
   movements.forEach((mov, i) => {
     const type = mov > 0 ? 'deposit' : 'withdrawal'
     const html = `
@@ -100,7 +161,52 @@ function displayMovements(movements) {
         </div>
     `
 
-    containerMovements.innerHTML = html
+    // containerMovements.innerHTML = html
     // insertAdjacentHTML
+    containerMovements.insertAdjacentHTML('afterbegin', html)
   })
+
+}
+
+btnTransfer.addEventListener('click', (e) => {
+  e.preventDefault()
+
+  const amount = Number(inputTransferAmount.value)
+  const receiverAcc = accounts.find(
+    (acc) => acc.username === inputTransferTo.value
+  )
+
+  inputTransferAmount.value = inputTransferTo.value = ''
+
+if (
+  amount > 0 &&
+  receiverAcc &&
+  currentAccount.balance >= amount &&
+  receiverAcc?.username !== currentAccount.username
+) {
+  currentAccount.movements.push(-amount)
+  receiverAcc.movements.push(amount)
+}
+})
+
+function startTimer(duration, display) {
+  var timer = duration, minutes, seconds;
+  if (minutes != '00' && seconds != '00') {
+    setInterval(function () {
+      minutes = parseInt(timer / 60, 10)
+      seconds = parseInt(timer % 60, 10);
+
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      display.textContent = minutes + ":" + seconds;
+
+      if (--timer < 0) {
+        timer = duration;
+      }
+    }, 1000);
+  } else if (minutes == '00' & seconds == '00') {
+    containerApp.style.opacity = 0
+    currentAccount = undefined
+  }
 }
